3cc9de1f124864382b36d154c1ebec6b1792ec11
Enes <raidenx231@gmail.com>
2023-12-12 12:56:57 +0300

    Update Home.jsx

--- client/src/app/Components/Home.jsx
+++ client/src/app/Components/Home.jsx
@@ -1,7 +1,8 @@
 "use client";
-import axios from "axios";
+
 import React, { useEffect, useState } from "react";
 import MapItems from "./MapItems";
+import axios from "axios";
 
 const HomePAGE = () => {
   const [allFilms, setAllFilms] = useState([]);
